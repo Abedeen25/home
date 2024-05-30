317eb4f2a2c9d1d07b23e455a0a201364a898d35
Iftekharul Abedeen <44981422+Abedeen25@users.noreply.github.com>
2024-05-30 12:01:07 +0600

    Fav

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-import logo from './logo.svg';
+import logo from './Assets/Images/iA.svg';
 import './App.css';
+import {Helmet} from "react-helmet";
 
 function App() {
   return (
     <div className="App">
+      <Helmet>
+        <meta charSet='utf-8' />
+        <title>Iftekharul Abedeen</title>
+        <link rel='canonical' href='http://google.com' />
+        <meta name='description' content='My portfolio' />
+      </Helmet>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
