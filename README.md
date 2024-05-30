12c5e71921d546a4a185d9b3458a333b5bb81b32
Iftekharul Abedeen <44981422+Abedeen25@users.noreply.github.com>
2024-05-30 22:06:26 +0600

    Outline created

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # home
+
+-- deploy using "npm run deploy"
+-- debug using "npm start"
--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './Assets/Images/iA.svg';
 import './App.css';
+import MainBody from './components/MainBody';
 import {Helmet} from "react-helmet";
 
 function App() {
@@ -11,20 +12,7 @@ function App() {
         <link rel='canonical' href='http://google.com' />
         <meta name='description' content='My portfolio' />
       </Helmet>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload hello.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <MainBody></MainBody>
     </div>
   );
 }
